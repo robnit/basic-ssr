38a70ba1755ead9b38b700f64022c5f4ef69d475
Robin Miller <rmillerpdx@gmail.com>
2021-11-07 13:29:06 -0800

    init

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import path from 'path';
+import fs from 'fs';
+
+import React from 'react';
+import express from 'express';
+import ReactDOMServer from 'react-dom/server';
+
+const app = express();
